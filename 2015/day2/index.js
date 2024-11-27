1bfbb1bf101a3c1b842d8c792607c50f3328c7c9
Vincent <vsuen123@gmail.com>
2024-11-27 16:18:33 -0500

    fix: day2: part1, part2

--- 2015/day2/index.js
+++ 2015/day2/index.js
@@ -1,17 +1,35 @@
 import fs from "fs";
-//surface area = 2*l*w + 2*w*h + 2*h*l
-//
-var l = null;
-var w = null;
-var h = null;
-l;
+//surface area = 2*l*w + 2*w*h + 2*h*
+
+let total = 0;
+
 fs.readFile("./input.txt", "utf8", (error, data) => {
   if (error) {
     console.log(error);
     return;
   }
-  for (let i = 0; i < data.length; i++) {
-    [l, w, h] = data.split("x");
+
+  const lines = data.trim().split("\n");
+
+  for (let i = 0; i < lines.length; i++) {
+    const [l, w, h] = lines[i].split("x");
+
+    //const a1 = l * w;
+    //const a2 = w * h;
+    //const a3 = h * l;
+
+    const [smallest1, smallest2] = [l, w, h].sort((a, b) => a - b);
+
+    const ribbon = 2 * smallest1 + 2 * smallest2;
+
+    const bow = eval(lines[i].replace(/x/g, "*"));
+
+    const feet = ribbon + bow;
+    total = feet + total;
+
+    //const surfaceArea = 2 * a1 + 2 * a2 + 2 * a3 + minSide;
+    //total = surfaceArea + total;
   }
-  console.log(l);
+
+  console.log(total);
 });
